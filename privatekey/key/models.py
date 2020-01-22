e35a5d3c467e8e0c20efb3ae83b5645e72c30e94
meowsir <meowsirrr@163.com>
2020-01-22 23:39:59 +0800

    delete some file

--- privatekey/key/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Account(models.Model):
-    name = models.CharField(max_length=20);
-    account = models.CharField(max_length=30);
-    password = models.CharField(max_length=30);
